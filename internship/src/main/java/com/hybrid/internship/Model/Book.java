0d2e64acdd99a1326fc2672d2d2d4e26311366e5
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-09-16 16:35:02 +0200

    Added book model

---
+++ internship/src/main/java/com/hybrid/internship/Model/Book.java
@@ -0,0 +1,15 @@
+package com.hybrid.internship.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String author;
+}

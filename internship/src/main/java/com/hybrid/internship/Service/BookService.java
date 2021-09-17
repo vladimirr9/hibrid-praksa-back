22086bd8046efd9266b4c8563b47f2fe4a72071e
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2021-09-17 16:20:58 +0200

    Fixed fetching only one book

--- internship/src/main/java/com/hybrid/internship/Service/BookService.java
+++ internship/src/main/java/com/hybrid/internship/Service/BookService.java
@@ -4,6 +4,7 @@ import com.hybrid.internship.Model.Book;
 import com.hybrid.internship.Repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -23,17 +24,15 @@ public class BookService {
     }
 
     public Book findById(Long id) {
-        return bookRepository.getById(id);
+        return bookRepository.findById(id).orElse(null);
     }
-
     public boolean delete(Long id) {
-        Book bookForDeletion = bookRepository.getById(id);
+        Book bookForDeletion = bookRepository.findById(id).get();
         bookRepository.delete(bookForDeletion);
         return true;
     }
-
     public boolean update(long id, Book newBook) {
-        Book bookForUpdate = bookRepository.getById(id);
+        Book bookForUpdate = bookRepository.findById(id).orElse(null);
         if (newBook.getAuthor() != null)
             bookForUpdate.setAuthor(newBook.getAuthor());
         if (newBook.getTitle() != null)
